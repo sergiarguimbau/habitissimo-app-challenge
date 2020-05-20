e91342452a9f8b90bdbfca43992d168653e0a1f4
Igresator <sergiarguimbau@hotmail.com>
2020-05-20 13:01:41 +0200

    Validate contact details

--- app/src/main/java/com/habitissimo/appchallenge/DialogFillContact.java
+++ app/src/main/java/com/habitissimo/appchallenge/DialogFillContact.java
@@ -3,9 +3,14 @@ package com.habitissimo.appchallenge;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -40,21 +45,101 @@ public class DialogFillContact extends DialogFragment {
         inputLayout_phone = (TextInputLayout) view.findViewById(R.id.inputlayout_phone);
         inputLayout_email = (TextInputLayout) view.findViewById(R.id.inputlayout_email);
 
+        // Update Text Input Layout error state for: name, phone, email
+        editText_name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                if(inputLayout_name.isErrorEnabled()){
+                    boolean valid_name = charSequence.toString().matches( "[a-zA-z]+([ '-][a-zA-Z]+)*" );
+                    inputLayout_name.setError(valid_name ? null : " ");
+                }
+
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {}
+
+        });
+
+        editText_phone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                if(inputLayout_phone.isErrorEnabled()) {
+                    boolean valid_phone = charSequence.toString().matches("(\\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}"); // Valid for Spain
+                    inputLayout_phone.setError(valid_phone ? null : " ");
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {}
+
+        });
+
+        editText_email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                if(inputLayout_email.isErrorEnabled()){
+                    boolean valid_email = Patterns.EMAIL_ADDRESS.matcher(charSequence.toString()).matches();
+                    inputLayout_email.setError(valid_email ? null : " ");
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {}
+
+        });
+
+        // Bu8ld dialog with custom view and two buttons
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setView(view);
         builder.setTitle(getActivity().getString(R.string.contact_details));
-        builder.setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+        builder.setPositiveButton(R.string.confirm, null); // Custom listener validates input
+        builder.setNegativeButton(android.R.string.cancel, null);
+
+        AlertDialog dialog = builder.create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                // TODO: Data Validation pending
-                String name = editText_name.getText().toString();
-                String phone = editText_phone.getText().toString();
-                String email = editText_email.getText().toString();
-                ((QuotationActivity)getActivity()).setFillContact(name, phone, email);
+            public void onShow(DialogInterface dialogInterface) {
+
+                // Prevent dialog dismiss with this Custom listener for the Positive Button
+                // to be capable of validating the input of the EditTexts: name, phone, email
+                Button button = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                button.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        // Data Validation
+                        String name = editText_name.getText().toString();
+                        String phone = editText_phone.getText().toString();
+                        String email = editText_email.getText().toString();
+
+                        boolean valid_name = name.matches( "[a-zA-z]+([ '-][a-zA-Z]+)*" );
+                        boolean valid_phone = phone.matches("(\\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}"); // Valid for Spain
+                        boolean valid_email = Patterns.EMAIL_ADDRESS.matcher(email).matches();
+
+                        inputLayout_name.setError(valid_name ? null : " ");
+                        inputLayout_phone.setError(valid_phone ? null : " ");
+                        inputLayout_email.setError(valid_email ? null : " ");
+
+                        if (valid_name && valid_phone && valid_email) {
+                            ((QuotationActivity)getActivity()).setFillContact(name, phone, email);
+                            dialog.dismiss();
+                        }else{
+                            Toast.makeText(getContext(), getString(R.string.contact_validate), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
-        builder.setNegativeButton(android.R.string.cancel, null);
-        return builder.create();
+
+        return dialog;
     }
 
 
